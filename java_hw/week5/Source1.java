3231d9059664fe054d03657f7dd5cb1893ab3eaf
sunneepark <seonhuibag1228@gmail.com>
2019-06-14 15:02:58 +0900

    19/06/14 upload cause of encoding

---
+++ java_hw/week5/Source1.java
@@ -0,0 +1,13 @@
+package ex05; 
+
+public class Source1 {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub 
+		Student s = new Student("ȫ�浿",1,1,100,60,76);
+		
+		System.out.println(s.getTotal());
+		System.out.println(s.getAverage());
+	}
+
+}  
